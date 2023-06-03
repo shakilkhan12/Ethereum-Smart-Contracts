95479df967cd2354b1c211f30b85c19c3bdc37dc
shakilkhan12 <shakilkhan621@gmail.com>
2023-06-03 07:52:38 +0500

    solidity units

---
+++ 07-units.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Units {
+    // Ether units;
+    uint public oneWei = 1 wei;
+    uint public oneGwei = 1 gwei;
+    uint public oneEther = 1 ether; 
+
+    // Time units;
+    uint public oneSecond = 1 seconds;
+    uint public oneMinute = 1 minutes;
+    uint public oneHour = 1 hours;
+    uint public oneDay = 1 days;
+    uint public oneWeek = 1 weeks;
+}
